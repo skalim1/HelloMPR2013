32be9652823e129cdf633af0e4f8c06e1496567d
skalim1 <skalim1@wp.pl>
2013-09-28 12:42:15 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
 
 	/**
@@ -6,7 +5,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 
 	}
 
